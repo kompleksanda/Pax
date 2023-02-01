4df50249130533a6def7b6d009df32c47047b74c
Alexander Akinyomola <46966054+kompleksanda@users.noreply.github.com>
2023-02-01 13:10:16 +0100

    h

--- paxmykro.js
+++ paxmykro.js
@@ -2,8 +2,7 @@ const { createPaxfulApi } = require('./papi.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const paxfulApi = createPaxfulApi("uqVqXXi7wwxCRWFB7Ss06OKmBHVuaCT80ZjensN9tiMPfmx4", "QTDAt71VKdBwFoXGIzuU4eEBf7RKK0LOEglTfo9INdOstxSN");
-//const paxfulApi = createPaxfulApi(process.env.PAXFUL_CLIENT_ID, process.env.PAXFUL_API_SECRET);
+const paxfulApi = createPaxfulApi(process.env.PAXFUL_CLIENT_ID, process.env.PAXFUL_API_SECRET);
 
 console.log(process.env.PAXFUL_CLIENT_ID);
 process.env.PAXFUL_API_SECRET
